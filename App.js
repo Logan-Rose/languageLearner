5329582f68426f54644f794bf3a54473552cc02c
Logan-Rose <logan.rose0329@gmail.com>
2023-08-30 00:56:29 -0400

    aded words with conjugation

--- App.js
+++ App.js
@@ -1,30 +1,58 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 import verbs from "./verbs.json";
+import words from "./words.json";
 import sampleSentences from "./sample.json";
 import { useState } from "react";
 
 export default function App() {
   const tmp = "Logan";
-  const frenchWords = Object.keys(sampleSentences);
-  const [french, setFrench] = useState("être");
-  const [english, setEnglish] = useState("to be");
-
+  const frenchWords = Object.keys(words);
+  const [french, setFrench] = useState("");
+  const [english, setEnglish] = useState("");
+  const [conjugation, setConjugation] = useState({});
   function updateWord() {
     const index = Math.floor(Math.random() * frenchWords.length);
     setFrench(frenchWords[index]);
-    setEnglish(sampleSentences[frenchWords[index]]);
+    setEnglish(words[frenchWords[index]].english);
+    setConjugation(words[frenchWords[index]].conjugation);
   }
 
   return (
     <View style={styles.container}>
       <View>
+        <Text style={{ color: "black", fontSize: "48px" }}>{french}</Text>
         <Button
           style={{ backgroundColor: "red" }}
-          title={french}
+          title="Translation"
           color="#841573"
           onPress={() => {
             alert(`${french}: ${english}`);
+          }}
+        ></Button>
+        <Button
+          style={{ backgroundColor: "red" }}
+          title="j"
+          color="#841573"
+          onPress={() => {
+            alert(`${conjugation.j}`);
+          }}
+        ></Button>
+        <Button
+          style={{ backgroundColor: "red" }}
+          title="tu"
+          color="#841573"
+          onPress={() => {
+            alert(`${conjugation.tu}`);
+          }}
+        ></Button>
+      </View>
+      <View>
+        <Button
+          style={{ backgroundColor: "red" }}
+          title="Nouveau Mot"
+          color="#841573"
+          onPress={() => {
             updateWord();
           }}
         ></Button>
